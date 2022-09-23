da5e497f0e026b0a00444d439425e9c7f6ffa991
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-09-23 18:07:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение m таблицы: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение n таблицы: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m,n];
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {        
+            array[i,j] = new Random().Next(0,100);      
+            Console.Write(array[i,j] + ", ");
+    }
+    Console.WriteLine();
+}
